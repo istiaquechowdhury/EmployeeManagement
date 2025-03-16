c7a9c692bd699c191cbbecf352f574d6939f6d82
istiaquechowdhury <istiaquechowdhury430@gmail.com>
2025-03-16 08:34:14 +0600

    hg

--- EmployeeManagement/EmployeeManagement.Web/obj/Debug/net8.0/EmployeeManagement.Web.AssemblyInfo.cs
+++ EmployeeManagement/EmployeeManagement.Web/obj/Debug/net8.0/EmployeeManagement.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3c57127162dfd2baff988df24cb5f7031e3d11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94469d53b9220be082d915fd59de23a62463b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
